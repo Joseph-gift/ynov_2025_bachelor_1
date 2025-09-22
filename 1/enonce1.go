f39558b9c9bffc15d1d0f019037828fd06c62438
gjosephm <josephmiyigbedo.godonou@ynov.com>
2025-09-22 19:00:55 +0200

    correction

--- 1/enonce1.go
+++ 1/enonce1.go
@@ -5,9 +5,15 @@ import "fmt"
 func main()  {
 
 	var age uint8
+	var nom string
+	var premon string
 
 	fmt.Print("Entre votre age: ")
 	fmt.Scan(&age)
+	fmt.Print("Entre votre Nom: ")
+	fmt.Scan(&nom)
+	fmt.Print("Entre votre Prenom: ")
+	fmt.Scan(&premon)
 	// si la personne est majeur
 	if age > 18{
 		// print  majeur
